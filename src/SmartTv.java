9f569d0dfc507459f9a28cdf6356c1383d2e1138
Leticia Neves <nevesleticia1516@icloud.com>
2024-05-26 23:58:47 -0300

    Initial commit

--- src/SmartTv.java
+++ src/SmartTv.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class SmartTv {
+public class SmartTv {
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 15;
+
+    public void ligar() {
+        ligada = true;
+    }
+
+    public void desligar() {
+        ligada = false;
+    }
+
+    public void aumentarVolume() {
+        volume++; //ou volume = volume + 1;
+    }
+
+    public void diminuirVolume() {
+        volume--; //ou volume = volume - 1;
+    }
+
+    public void mudarCanal (int novoCanal){
+        canal = novoCanal;
+    }
+
+    public void passarCanal(){
+        canal++;
+    }
+
+    public void voltarCanal(){
+        canal--;
+    }
 }
+
--- src/Usuario.java
+++ src/Usuario.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Usuario {
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV Ligada? " + smartTv.ligada); //false
+        System.out.println("Canal atual: " + smartTv.canal); //1
+        System.out.println("Volume atual: " + smartTv.volume); //15
+
+        smartTv.ligar();
+        System.out.println("New states - TV Ligada? " + smartTv.ligada); //true
+
+        smartTv.desligar();
+        System.out.println("New states - TV Ligada? " + smartTv.ligada); //false
+
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.diminuirVolume();
+        System.out.println("Volume atual: " + smartTv.volume); //17
+
+        smartTv.mudarCanal(27);
+        System.out.println("O canal atual é: " + smartTv.canal); //27 (usuario passando como parametro o canal desejado)
+
+        smartTv.passarCanal(); //28
+        smartTv.passarCanal(); //29
+        smartTv.passarCanal(); //30
+        smartTv.passarCanal();
+        System.out.println("O canal atual é: " + smartTv.canal); //31
+
+    }
 }
